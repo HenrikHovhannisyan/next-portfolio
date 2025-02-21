b9c61d60037d0e49ab3add6f079ae065523ca27e
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-02-21 14:38:26 +0400

    Added Sidebar component

--- components/Particle.tsx
+++ components/Particle.tsx
@@ -11,73 +11,73 @@ const Particle = () => {
 
   const particlesLoaded = useCallback(async () => {}, []);
   return (
-      <Particles
-          className="h-screen"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-            fullScreen: { enable: false },
-            fpsLimit: 120,
-            interactivity: {
-              events: {
-                onHover: {
-                  enable: true,
-                  mode: "repulse",
-                },
-                resize: true,
-              },
-              modes: {
-                repulse: {
-                  distance: 100,
-                  duration: 0.4,
-                },
-              },
+    <Particles
+      className="h-screen"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: { enable: false },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onHover: {
+              enable: true,
+              mode: "repulse",
             },
+            resize: true,
+          },
+          modes: {
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
+          },
+        },
 
-            particles: {
-              color: {
-                value: "#EE0F0F",
-              },
-              links: {
-                color: "#EE0F0F",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 2,
-              },
-              collisions: {
-                enable: true,
-              },
-              move: {
-                enable: true,
-                direction: "top-left",
-                outModes: {
-                  default: "bounce",
-                },
-                random: true,
-                straight: false,
-                speed: 1,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 105,
-              },
-              opacity: {
-                value: 0.5,
-              },
-              shape: {
-                type: "circle",
-              },
-              size: {
-                value: { min: 1, max: 5 },
-              },
+        particles: {
+          color: {
+            value: "#EE0F0F",
+          },
+          links: {
+            color: "#EE0F0F",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 2,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            enable: true,
+            direction: "top-left",
+            outModes: {
+              default: "bounce",
             },
-            detectRetina: true,
-          }}
-      />
+            random: true,
+            straight: false,
+            speed: 1,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 105,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
   );
 };
 
